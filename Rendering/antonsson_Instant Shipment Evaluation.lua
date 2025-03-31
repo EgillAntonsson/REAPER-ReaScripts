a4337b5276befe7f92c33ea1425632ee4ac8999e
Egill Antonsson <6976127+EgillAntonsson@users.noreply.github.com>
2025-03-31 08:47:24 +0200

    updated repo name in index.xml. Added Shipment jsfx and lua script. Deleted the example script.

---
+++ Rendering/antonsson_Instant Shipment Evaluation.lua
@@ -0,0 +1,20 @@
+-- @desccription Instant Shipment Evaluation
+-- @author Egill Antonsson
+-- @version 0.0.1
+-- @changelog: First release
+-- @link Website https://www.egill.rocks/blog/rea-scripts/shipment-evaluator
+-- @donation: Donate via PayPal https://www.paypal.com/donate/?hosted_button_id=9PSFWJT92K8U4
+-- @about
+--   # The Instant Shipment Evaluation by Egill Antonsson
+--
+--   ## Disclaimer
+--   Use of this software is on your own risk as it provides a powerful encouragement to "SHIP IT !",
+--
+--   Thus you need to be careful about when to use it (not too soon, not too late).
+--
+--   This software is completely predictable as it has a fixed evaluation result and does no audio processing.
+--
+-- @tags: analysis, utility, evaluation, encouragement, rendering
+-- @license: MIT (Do whatever you like with this code)
+
+reaper.ShowConsoleMsg("\nInstant Shipment Evaluation Result: ♬ ♫ ♪ ♩  -->  SHIP IT !  <--  ♩ ♪ ♫ ♬\n")
